63cb0644ebed94522c5755af14585bc71d9f76b4
Fredrik Welander <fredrik.welander@subsite.fi>
2023-03-31 22:22:45 +0300

    Add CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os, json
 import openai
 from flask import Flask, request
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 authorized_keys = (os.getenv("API_KEY", "") + "," + os.getenv("USER_KEYS", "")).split(",")
